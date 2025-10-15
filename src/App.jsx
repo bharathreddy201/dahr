62716aa3c7b4de22ea1961c8bc7892ec190dabc0
bharathreddy20202-oss <bharathreddy20202@gmail.com>
2025-10-15 16:05:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { NeumorphicHRMSDashboard } from './components/NeumorphicHRMSDashboard.jsx';
+
+export default function App() {
+  return (
+    <div className="size-full">
+      <NeumorphicHRMSDashboard />
+    </div>
+  );
+}
